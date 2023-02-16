3f1a2244f64672deef534dd9cde9b30293bbfbe8
RadonRebirth <96480522+RadonRebirth@users.noreply.github.com>
2023-02-16 08:09:12 +0300

    Add files via upload

---
+++ cSHARPlab1/cSHARPlab1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace cSHARPlab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) 
+            { 
+                double d = 3.14 / 2;
+                double summ = 0;
+
+                Console.WriteLine("Введите значение n:");
+                int n = Convert.ToInt32(Console.ReadLine());
+                if (n < 0)
+                {
+                    Console.WriteLine("Вы ввели неверное значение!");
+                    break;
+                }
+
+                Console.WriteLine("Введите значение x:");
+                int x = Convert.ToInt32(Console.ReadLine());
+                if (x > 1)
+                {
+                    Console.WriteLine("Вы ввели неверное значение!");
+                    break;
+                }
+
+                for (var j = 0; j <= n; j++)
+                {
+                    double arcctgx = (Math.Pow(-1, n + 1)) * (Math.Pow(x, 2 + n + 1)) / 2 * n + 1;
+                    summ = summ + arcctgx;
+                }
+
+                d = summ + d;
+
+                Console.WriteLine($"Результат выражения: {d}");
+            }
+        }
+    }
+}
